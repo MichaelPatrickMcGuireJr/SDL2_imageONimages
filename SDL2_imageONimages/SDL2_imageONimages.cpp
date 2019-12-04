ab44420e0d24d6dada7c144a76b6bb148c866af3
Michael McGuire <embedded.engineer@outlook.com>
2019-12-04 14:19:40 -0800

    cleaned up and added comments to SDL2_imageONimages.cpp

--- SDL2_imageONimages/SDL2_imageONimages.cpp
+++ SDL2_imageONimages/SDL2_imageONimages.cpp
@@ -1,25 +1,27 @@
-// SDL_tutorial2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ *   Author:  Michael McGuire
+ *  Purpose:  SDL2, openning the direct media layer and collating an image on the background, then centering a different image on top
+ * Language:  c++
+ */
 
+// headers
 #include <iostream>
-
-#include "Cleanup.h"
-
 #include "SDL.h"
 
+// custom header to help deallocate SDL2 memory usage
+#include "Cleanup.h"
 
+// allows us to implicitly call the std namespace from iostream
+using namespace std;
 
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-// standard error logger
-void logSDLError(std::ostream& os, const std::string& msg) {
+// function to stream error messages 
+void logSDLError(ostream& os, const string& msg) 
+{
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
-
-SDL_Texture* loadTexture(const std::string& file, SDL_Renderer* ren) {
+// function to load image file onto surface, then copy to and return texture
+SDL_Texture* loadTexture(const string& file, SDL_Renderer* ren) {
 	//Initialize to nullptr to avoid dangling pointer issues
 	SDL_Texture* texture = nullptr;
 	//Load the image
@@ -39,89 +41,106 @@ SDL_Texture* loadTexture(const std::string& file, SDL_Renderer* ren) {
 	return texture;
 }
 
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-*/
+// function to place image on renderer
 void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, int x, int y) {
-	//Setup the destination rectangle to be at the position we want
+	// create rectangle
 	SDL_Rect dst;
+	// position rectangle
 	dst.x = x;
 	dst.y = y;
-	//Query the texture to get its width and height to use
+	// place image on texture
 	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	// copy rectangle with textured image on renderer
 	SDL_RenderCopy(ren, tex, NULL, &dst);
 }
 
 int main(int argc, char* argv[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		logSDLError(std::cout, "SDL_Init");
+	// initialize SDL2, end program if error initializing SDL2
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+	{
+		// send error message to console
+		logSDLError(cout, "SDL_Init");
+		// end main
 		return 1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Lesson 2", 100, 100, SCREEN_WIDTH,
-		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		logSDLError(std::cout, "CreateWindow");
+	// create window using SDL2
+	int window_width = 640; int window_height = 480;
+	SDL_Window* window = SDL_CreateWindow("Lesson 2", 100, 100, window_width, window_height, SDL_WINDOW_SHOWN);
+	// if error creating window
+	if (window == nullptr) 
+	{
+		// send error message to console
+		logSDLError(cout, "CreateWindow");
+		// destroy SDL2
 		SDL_Quit();
+		// end main
 		return 1;
 	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
-		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		logSDLError(std::cout, "CreateRenderer");
+	
+	// create graphics renderer for SDL2
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	// if error creating renderer
+	if (renderer == nullptr) 
+	{
+		// send error message to console
+		logSDLError(cout, "CreateRenderer");
+		// deallocate memory from window creation
 		cleanup(window);
+		// destroy SDL2
 		SDL_Quit();
+		// end main
 		return 1;
 	}
 
 
-	//const std::string resPath = getResourcePath("Lesson2");
+	//create 2 texture holders: 1 with background image and 1 with foreground image
 	SDL_Texture* background = loadTexture("background.bmp", renderer);
-	SDL_Texture* image = loadTexture("image.bmp", renderer);
-	if (background == nullptr || image == nullptr) {
-		cleanup(background, image, renderer, window);
+	SDL_Texture* foreground = loadTexture("image.bmp", renderer);
+	// if error creating either texture
+	if (background == nullptr || foreground == nullptr) 
+	{
+		// deallocate memory from background, foreground, renderer, and the window
+		cleanup(background, foreground, renderer, window);
+		// destroy SDL2
 		SDL_Quit();
+		// end main
 		return 1;
 	}
 
+	// clear the renderer
 	SDL_RenderClear(renderer);
 
+	// setup the background
 	int bW, bH;
+	// resize the background texture to the size of background image
 	SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+	// collate the background texture onto the renderer for the bacground 
 	renderTexture(background, renderer, 0, 0);
 	renderTexture(background, renderer, bW, 0);
 	renderTexture(background, renderer, 0, bH);
 	renderTexture(background, renderer, bW, bH);
 
-
+	// place foreground image on top
 	int iW, iH;
-	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
-	int x = SCREEN_WIDTH / 2 - iW / 2;
-	int y = SCREEN_HEIGHT / 2 - iH / 2;
-	renderTexture(image, renderer, x, y);
+	// resize the foreground texture to the size of foreground image
+	SDL_QueryTexture(foreground, NULL, NULL, &iW, &iH);
+	// draw the foreground image on top of and centered on the window
+	int x = window_width / 2 - iW / 2;
+	int y = window_height / 2 - iH / 2;
+	renderTexture(foreground, renderer, x, y);
 
+	// copy the rendered image to the window
 	SDL_RenderPresent(renderer);
-	SDL_Delay(1000);
 
-	cleanup(background, image, renderer, window);
+	// wait 10 seconds
+	SDL_Delay(10000);
+	// deallocate memory from the Textures, the Renderer, and the window
+	cleanup(background, foreground, renderer, window);
+	// destroy SDL2
 	SDL_Quit();
 
+	// end main
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
